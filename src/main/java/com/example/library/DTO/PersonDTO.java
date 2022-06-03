4df2ba11d42930c18754f5c466f503b52caec940
Щерба Кирилл Сергеевич <KSShcherba@sberbank.ru>
2022-06-03 14:23:42 +0300

    Пост метод для регестрации читателя.

---
+++ src/main/java/com/example/library/DTO/PersonDTO.java
@@ -0,0 +1,14 @@
+package com.example.library.DTO;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+    private String firstNamePerson;
+
+    private String lastNamePerson;
+
+    private String numberPhone;
+
+    private Integer numberOfStud;
+}
--- src/main/java/com/example/library/controllers/RegistryPersonController.java
+++ src/main/java/com/example/library/controllers/RegistryPersonController.java
@@ -1,9 +1,13 @@
 package com.example.library.controllers;
 
+import com.example.library.DTO.PersonDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -11,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RegistryPersonController {
 
     @GetMapping()
-    public String registryPerson()
-    {
+    public String registryPerson(Model model) {
+        model.addAttribute("person", new PersonDTO());
         return "registry_new_person";
     }
+
+    @PostMapping()
+    public String registryNewPerson(@RequestParam String lastNamePerson,
+                                    @RequestParam String firstNamePerson,
+                                    @RequestParam String secondNamePerson,
+                                    @RequestParam Integer numberStudTicket,
+                                    @RequestParam String phonePerson, Model model){
+        System.out.println(lastNamePerson);
+        return null;
+    }
+
 }
